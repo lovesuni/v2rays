83706604c621713b2fb241e5af17b6e64b9632a9
lovesuni <lovesuni@126.com>
2018-11-10 08:35:19 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
-#cd /v2raybin
-#echo -e "$CONFIG_JSON" > config.json
-#if [ "$CERT_PEM" != "$KEY_PEM" ]; then
-#  echo -e "$CERT_PEM" > cert.pem
-#  echo -e "$KEY_PEM"  > key.pem
-#fi
+cd /v2raybin
+echo -e "$CONFIG_JSON" > config.json
+if [ "$CERT_PEM" != "$KEY_PEM" ]; then
+  echo -e "$CERT_PEM" > cert.pem
+  echo -e "$KEY_PEM"  > key.pem
+fi
 
 cd /v2ray
 wget -O v2ray.zip https://github.com/v2ray/v2ray-core/releases/download/v4.2/v2ray-linux-64.zip
